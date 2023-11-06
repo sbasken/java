bc6330d7e7648c867c118e2aeacd8ca92ab1a1f7
sbasken <120995506+sbasken@users.noreply.github.com>
2023-11-06 13:54:49 -0800

    Update Droid.java

--- Droid.java
+++ Droid.java
@@ -1,1 +1,30 @@
+public class Droid {
 
+  String name;
+  int batteryLevel;
+
+  public Droid(String droidName) {
+    name = droidName;
+    batteryLevel = 100;
+  }
+
+  public String toString(){
+    return "Hello, I’m the droid: " + name;
+  }
+
+  public void performTask(String task){
+    System.out.println(name + " is performing task: " + task);
+    batteryLevel -= 10;
+  }
+
+  public void energyReport(){
+    System.out.println(name + "'s battery Level is at " + batteryLevel + "%.");
+  }
+
+  public static void main(String[] args) {
+    Droid codey = new Droid("Codey");
+    System.out.println(codey);
+    codey.performTask("dancing");
+    codey.energyReport();
+  }
+}
