dac6baf356a6b96fd3b84b8875275510e240f7d7
sbasken <120995506+sbasken@users.noreply.github.com>
2023-11-07 14:12:08 -0800

    Update CarLoan.java

--- CarLoan.java
+++ CarLoan.java
@@ -1,20 +1,20 @@
-// Simple loan monthly payment calculator (does not account for the compounding of interest over the months)
+// Simple loan monthly payment estimator
 
 public class CarLoan {
 	public static void main(String[] args) {
-    int carLoan = 10000;
-    int loanLength = 3;
-    int interestRate = 5;
-    int downPayment = 2000;
+	    int carLoan = 10000;
+	    int loanLength = 3;
+	    int interestRate = 5;
+	    int downPayment = 2000;
+		
+	    if (loanLength <= 0 || interestRate <= 0) {
+	      System.out.println("Error! You must take out a valid car loan.");
+	    } else if (downPayment > carLoan) {
+	      System.out.println("he car can be paid in full.");
+	    } else {
+	      double monthlyPayment = (double)(carLoan - downPayment) * (interestRate / 100.0 + 1) / (loanLength * 12);
 	
-    if (loanLength <= 0 || interestRate <= 0) {
-      System.out.println("Error! You must take out a valid car loan.");
-    } else if (downPayment > carLoan) {
-      System.out.println("he car can be paid in full.");
-    } else {
-      double monthlyPayment = (double)(carLoan - downPayment) * (interestRate / 100.0 + 1) / (loanLength * 12);
-
-      System.out.println((int)monthlyPayment);
-    }
+	      System.out.println((int)monthlyPayment);
+	    }
 	}
 }
